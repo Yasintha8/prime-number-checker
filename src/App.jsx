9a320572adec42033479dd1dfd64504888835436
Yasintha28 <yasinthachamikara28@gmail.com>
2025-03-30 09:30:12 +0530

    css change

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
             onClick={checkPrime}>
             Check
           </button>
-          <button className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400 transition"
+          <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
           onClick={clearInput}>
             Clear
           </button>
